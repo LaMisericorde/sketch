372f99122b5eec7b8079b72f0d613da3c0808aae
Friedrich Erbach <friedrich.erbach@protonmail.com>
2022-05-10 11:48:27 +0300

    The size of the grid can be changed and the squares change color ( a fixed color)

--- main.js
+++ main.js
@@ -1,10 +1,37 @@
 const container = document.querySelector('.container');
 const gridSizeInput = document.getElementById('grid-size');
-const sizeText = document.querySelector('.size');
-let gridSize;
-gridSizeInput.addEventListener('click', e => {
+const writeSize = document.getElementById('write-size');
+let gridSize = 16;
+createDivs();
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+function changeColor(e) {
+    if (e.type === 'mouseover' && !mouseDown) {
+        return;
+    } else {
+        e.target.style.backgroundColor = "yellow";
+    }
+}
+
+gridSizeInput.addEventListener('input', e => {
     gridSize = e.target.value;
-    sizeText.textContent = gridSize;
+    writeSize.value = gridSize
+    removeOldDivs();
+    createDivs();
+})
+
+writeSize.addEventListener('change', e => {
+    gridSize = e.target.value;
+    gridSizeInput.value = gridSize;
+    if (gridSize < 10 || gridSize > 100) {
+        alert("Choose a number between 9 and 101");
+        gridSize = 16;
+        gridSizeInput.value = 16;
+        writeSize.value = 16;
+    }
     removeOldDivs();
     createDivs();
 })
@@ -14,9 +41,10 @@ function createDivs() {
         const cell = document.createElement('div');
         cell.setAttribute('class', 'cell');
         cell.setAttribute('id', 'n' + i);
-        cell.textContent = i;
         container.appendChild(cell);
         container.style.setProperty('--size', gridSize);
+        cell.addEventListener('mouseover', changeColor);
+        cell.addEventListener('mousedown', changeColor);
     }
 }
 
